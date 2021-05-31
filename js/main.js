a7ed9ddf3b90637344d0f8a056a90be70f7b3f33
Alex <al.lyzhin@gmail.com>
2021-06-01 00:42:34 +0300

    Корректировки для второй проверки

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
 const getRandomInteger = (min, max) => {
-  if (min < 0) {
-    min = 0;
+  if (min < 0 || max < 0) {
+    // eslint-disable-next-line no-console
+    console.error('Аргументы должны быть больше или равны 0');
+    return;
   }
-  if (max < 0) {
-    max = 0;
-  }
-
   if (min > max) {
-    let temp = min;
-    min = max;
-    max = temp;
-    temp = null;
+    // eslint-disable-next-line no-console
+    console.error('Минимум не должен быть больше максимума');
+    return;
   }
   const rand = min + Math.random() * (max + 1 - min); //https://learn.javascript.ru/task/random-int-min-max
   return Math.floor(rand);
 };
 
+const isValidLength = (string, maxLength) => string.length <= maxLength;
+
 getRandomInteger(1,10);
-
-const isValidLength = (string, maxLength) => string.length < maxLength;
-
 isValidLength('КЕК', 140);
