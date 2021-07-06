f1be5cb318e0c05019421c682308353cadbde1ed
Alex <al.lyzhin@gmail.com>
2021-07-06 22:02:00 +0300

    Меньше повторяющегося кода + исправлен debounce

--- js/filter-photos.js
+++ js/filter-photos.js
@@ -4,58 +4,63 @@ import {debounce} from './utils/debounce.js';
 
 const filtersForm = document.querySelector('.img-filters__form');
 const filterButtons = document.querySelectorAll('.img-filters__button');
-const defaultButton = filtersForm.querySelector('#filter-default');
-const randomButton = filtersForm.querySelector('#filter-random');
-const discussedButton = filtersForm.querySelector('#filter-discussed');
+const RERENDER_DELAY = 500;
 const PHOTOS_MIN_ID = 0;
 const RANDOM_PHOTOS_NUMBER = 10;
+const DEFAULT_FILTER = 'filter-default';
+let selectedFilter = DEFAULT_FILTER;
 
 const changeButtonsStyle = (evt) => filterButtons.forEach((btn) => btn.classList.toggle('img-filters__button--active', btn === evt.target));
 
-const removeCurrentPhotos = (container) => {
-  const photos = container.querySelectorAll('.picture');
+const removeCurrentPhotos = () => {
+  const photos = picturesContainerElement.querySelectorAll('.picture');
   photos.forEach((photo) => photo.remove());
 };
 
+const changeFilter = (evt, renderingPhotos) => {
+  if (selectedFilter !== evt.target.id) {
+    removeCurrentPhotos();
+    renderPhotos(renderingPhotos);
+    changeButtonsStyle(evt);
+    selectedFilter = evt.target.id;
+  }
+};
+
 const setImgFilters = (photos) => {
-  let selectedFilter = 'default';
   let photosMaxId = PHOTOS_MIN_ID;
   photos.forEach((photo) => photosMaxId = Math.max(photo.id, photosMaxId));
 
-  const onDefaultBtnClick = (evt) =>  {
-    if (selectedFilter !== 'default') {
-      removeCurrentPhotos(picturesContainerElement);
-      changeButtonsStyle(evt);
-      renderPhotos(photos);
-      selectedFilter = 'default';
+  const setDefaultFilter = (evt) => {
+    changeFilter(evt, photos);
+  };
+
+  const setRandomFilter = (evt) => {
+    const uniqueIntegerList = getRandomUniqueIntegerList(PHOTOS_MIN_ID, photosMaxId, RANDOM_PHOTOS_NUMBER);
+    const randomPhotos = photos.filter((photo) => uniqueIntegerList.includes(photo.id));
+    changeFilter(evt, randomPhotos);
+  };
+
+  const setDiscussedFilter = (evt) => {
+    const photosCloned = [...photos];
+    photosCloned.sort((a, b) => b.comments.length - a.comments.length);
+    changeFilter(evt, photosCloned);
+  };
+
+  const onFilterFormClick = (evt) => {
+    const filterButton = evt.target;
+    switch (filterButton.id) {
+      case 'filter-default':
+        setDefaultFilter(evt);
+        break;
+      case 'filter-random':
+        setRandomFilter(evt);
+        break;
+      case 'filter-discussed':
+        setDiscussedFilter(evt);
     }
   };
 
-  const onRandomBtnClick = (evt) => {
-    if (selectedFilter !== 'random') {
-      removeCurrentPhotos(picturesContainerElement);
-      changeButtonsStyle(evt);
-      const uniqueIntegerList = getRandomUniqueIntegerList(PHOTOS_MIN_ID, photosMaxId, RANDOM_PHOTOS_NUMBER);
-      const randomPhotos = photos.filter((photo) => uniqueIntegerList.includes(photo.id));
-      renderPhotos(randomPhotos);
-      selectedFilter = 'random';
-    }
-  };
-
-  const onDiscussedBtnClick = (evt) => {
-    if (selectedFilter !== 'discussed') {
-      removeCurrentPhotos(picturesContainerElement);
-      changeButtonsStyle(evt);
-      const photosCloned = [...photos];
-      photosCloned.sort((a, b) => b.comments.length - a.comments.length);
-      renderPhotos(photosCloned);
-      selectedFilter = 'discussed';
-    }
-  };
-
-  defaultButton.addEventListener('click', debounce(onDefaultBtnClick));
-  randomButton.addEventListener('click', debounce(onRandomBtnClick));
-  discussedButton.addEventListener('click', debounce(onDiscussedBtnClick));
+  filtersForm.addEventListener('click', debounce(onFilterFormClick, RERENDER_DELAY));
 };
 
 export {setImgFilters};
